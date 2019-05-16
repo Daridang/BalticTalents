d74517489e70713cf4a24c651fb53a928c6159f0
Daridang <daridang@gmail.com>
2019-05-16 18:13:22 +0300

    p0516 -> Exception, ir uzduotis is paskaitos

---
+++ src/main/java/p0516/ErrorCode.java
@@ -0,0 +1,13 @@
+package p0516;
+
+public enum ErrorCode {
+    HUNGRY("I'm very hungry!");
+    private String msg;
+    ErrorCode(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
